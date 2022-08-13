0a4f7b94a4705516e21ebd437c20ca789faef7fe
Hayin Ananta <hayin.10119005@mahasiswa.unikom.ac.id>
2022-08-13 16:02:23 +0700

    UAS AKB

---
+++ app/src/main/java/com/example/dailylife_10119005/SignUpActivity.java
@@ -0,0 +1,74 @@
+package com.example.dailylife_10119005;
+//NIM : 10119005
+//Nama : Hayin Ananta
+//Kelas : IF-1
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignUpActivity extends AppCompatActivity {
+    private EditText inputEmail, inputPassword;
+    private Button btnSignIn, btnSignUp;
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        ActionBar ab = getSupportActionBar();
+        if (ab != null)  ab.setTitle("Sign Up");
+
+        auth = FirebaseAuth.getInstance();
+
+        inputEmail = (EditText) findViewById(R.id.sign_up_email);
+        inputPassword = (EditText) findViewById(R.id.sign_up_password);
+        btnSignIn = (Button) findViewById(R.id.btn_sign_in);
+        btnSignUp = (Button) findViewById(R.id.btn_goto_sign_up);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = inputEmail.getText().toString().trim();
+                String password = inputPassword.getText().toString().trim();
+                if (email.isEmpty()) inputEmail.setError("email harus diisi!");
+                else if (password.isEmpty()) inputPassword.setError("password harus diisi!");
+                else if (password.length() < 6) inputPassword.setError("password harus > 6");
+                else {
+                    auth.createUserWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(SignUpActivity.this, "User dengan Email & Password berhasil dibuat", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(SignUpActivity.this, SignInActivity.class));
+                                    } else {
+                                        Toast.makeText(SignUpActivity.this, "Otentikasi Gagal", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+
+                }
+            }
+        });
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignUpActivity.this, SignInActivity.class));
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/dailylife_10119005/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/dailylife_10119005/ui/profile/ProfileFragment.java
@@ -4,11 +4,14 @@ package com.example.dailylife_10119005.ui.profile;
 //Nama : Hayin Ananta
 //Kelas : IF-1
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,6 +19,9 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.dailylife_10119005.R;
+import com.example.dailylife_10119005.SignInActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 //import com.example.dailylife_10119005.databinding.FragmentHomeBinding;
 
@@ -37,11 +43,53 @@ public class ProfileFragment extends Fragment {
 //    }
 
 
+    private Button btn_reset_password, btn_logout;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser firebaseUser;
+    private FirebaseAuth.AuthStateListener authStateListener;
+
     @Override
     public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        btn_logout = view.findViewById(R.id.btn_keluar);
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                firebaseUser = firebaseAuth.getCurrentUser();
+                if (firebaseUser == null) {
+                    startActivity(new Intent(getActivity(), SignInActivity.class));
+                }
+            }
+        };
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firebaseAuth.signOut();
+                Toast.makeText(getContext(), "Keluar", Toast.LENGTH_SHORT).show();
+//                startActivity(new Intent(getActivity(), SignInActivity.class));
+            }
+        });
+//        btn_logout.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                firebaseAuth.signOut();
+//                Toast.makeText(getContext(), "Berhasil keluar.", Toast.LENGTH_SHORT).show();
+//                startActivity(new Intent(getActivity(), SignInActivity.class));
+//            }
+//        });
         return view;
     }
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//        firebaseAuth.addAuthStateListener(authStateListener);
+//    }
+//
+//    @Override
+//    public void onStop() {
+//        super.onStop();
+//        firebaseAuth.removeAuthStateListener(authStateListener);
+//    }
 
     @Override
     public void onDestroyView() {
